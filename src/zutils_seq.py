5d46c49778d597124e90e5805aaa7397eeb2540f
Max Coursey <mrc30363@gmail.com>
2021-03-16 15:11:21 -0400

    create new seq zutils - working - added flag to pub socket to prevent old reconnects

--- src/zutils_seq.py
+++ src/zutils_seq.py
@@ -24,6 +24,8 @@ class Proxy():
     def __init__(self, in_bound=5555, out_bound=5556):
         self.in_bound = in_bound
         self.out_bound = out_bound
+        self.front_end = None
+        self.back_end = None
         self.path = '/proxy'
         self.elect_root_path = '/election'
         self.elect_candidate_path = '/election/candidate_sessionid_'
@@ -33,11 +35,11 @@ class Proxy():
         self.seq_id = None
         self.ip = get_ip()
         self.zk = start_kazoo_client()
-        self.id = str(randrange(1000,10000))
+        self.leader = None
+        self.leader_node = None
         self.isleader = False
         self.context = zmq.Context()
 
-    def start(self):
         print(f'New Proxy Created: {self.ip}')
         
         #Create znode for each instance of this class
@@ -50,18 +52,19 @@ class Proxy():
             newNode = self.zk.create(self.path, value=b'', ephemeral=True, makepath=True)
         
         #get all children under /election parent
-        candidate_list = self.zk.get_children(self.elect_root_path)
+        self.candidate_list = self.zk.get_children(self.elect_root_path)
         
         #candidate list removes parent node (/election/) from name
         #candidate list must be sorted sequentially
-        candidate_list.sort()
-        print(f'Candidate ZNode List: {candidate_list}')
+        self.candidate_list.sort()
+        print(f'Candidate ZNode List: {self.candidate_list}')
         
-        leader_node=""
+    def start(self):
         
         #Current Znode becomes LEADER
-        if self.my_path.endswith(candidate_list[0]):
-            leader_node = self.my_path
+        if self.my_path.endswith(self.candidate_list[0]):
+            self.leader_node = self.my_path
+            self.isleader = True
             print(f'Node is LEADER: {self.my_path}')
             print(f'My IP: {self.ip}')
             
@@ -71,48 +74,82 @@ class Proxy():
             self.zk.set(self.my_path, encoded_ip)
             
             #Set up zmq proxy socket
-            front_end = self.context.socket(zmq.XSUB)
-            front_end.bind(f"tcp://*:{self.in_bound}")
-            back_end = self.context.socket(zmq.XPUB)
-            back_end.setsockopt(zmq.XPUB_VERBOSE, 1)
-            back_end.bind(f"tcp://*:{self.out_bound}")            
+            self.front_end = self.context.socket(zmq.XSUB)
+            self.front_end.bind(f"tcp://*:{self.in_bound}")
+            self.back_end = self.context.socket(zmq.XPUB)
+            self.back_end.setsockopt(zmq.XPUB_VERBOSE, 1)
+            self.back_end.bind(f"tcp://*:{self.out_bound}")            
             print(
-                f"Broker {self.id} is leader - started w/ in_bound={self.in_bound}, out_bound={self.out_bound}")            
-            zmq.proxy(front_end, back_end)
+                f"Broker {self.my_path} is leader - started w/ in_bound={self.in_bound}, out_bound={self.out_bound}")            
+            zmq.proxy(self.front_end, self.back_end)
         
         
         #Current Znode becomes FOLLOWER
         else:
             
-            def watch_prev_node(event):
-                self.zk.exists(prev_node_path, watch_prev_node)
-                print(f" - Leader watcher triggered:\n\
-                      data:{data}, stat={stat}\n")
-                print(f'EVENT: {event}')
+            def watch_prev_node(prev_node):
+                 @self.zk.DataWatch(path=prev_node)
+                 def proxy_watcher(data, stat, event):
+
+                    if event is not None:
+                        if event.type == "DELETED":
+                            print(f'EVENT: {event}')
+
+                        # print(f'Current Leader node status: {self.zk.exists(path=self.path)}')
+                            election = self.zk.Election('election/', self.ip)
+                            contenders = election.contenders()
+                        # print(f'Contenders:\n{contenders}')
+                        # ##run() will block until election won - then call elect_leader func
+                            election.run(self.won_election) # blocks until wins or canceled
+                    else:
+                        print("Leader is existing - else stmt")
             
-                if event.type == "DELETED":
-                    self.start()
                     
-                elif event.type == "CHANGED":
-                    print(f'Changed: LeaderNode[0]: {leader_node[0]}')
-                    self.zk.set(self.my_path, leader_node[0])
+    ################        
             #make current node the follower    
-            print(f'Node is FOLLOWER: {self.my_path}')
-            leader_node = candidate_list[0]
+            print(f'Node is FOLLOWER')
+            self.isleader = False
+            leader_node = self.candidate_list[0]
+            print(f'LEADER IS: {leader_node}')
             
             #TODO - remove hotfix - sliced my_path to remove "/election/"
-            crnt_node_idx = candidate_list.index(self.my_path[10:])
+            crnt_node_idx = self.candidate_list.index(self.my_path[10:])
             print(f'Current Node Index: {crnt_node_idx}')
             
             #get node path of largest sequential node smaller than current - ensure sorted list above
-            prev_node_path = candidate_list[crnt_node_idx - 1]
+            prev_node_path = self.candidate_list[crnt_node_idx - 1]
             print(f'PrevNodePath: {prev_node_path}')
             
             prev_node = self.zk.exists(self.elect_root_path + "/" + prev_node_path, watch_prev_node)
             #TODO - either use DataWatch or get method, which instantiates watch in 2nd argument
             print(f'PreviousNode: {prev_node}')
+            while self.isleader is False:
+                watch_prev_node(self.elect_root_path + "/" + prev_node_path)
+                time.sleep(1)
 
+    def won_election(self): 
+        print(" - I'm going to be new leader")
+        print(self.path)
+        #check leader barrier node
+        if self.zk.exists(self.path) is None:
+            self.zk.create(self.path, value=self.ip.encode(
+                    'utf-8'), ephemeral=True, makepath=True)   
+            print(f' - Broker {self.ip} status: Leader')
+            ##Flag used to differentiate recieve message and state replication funcs
+            self.isleader = True
 
+            ##Set up xpub/xsub sockets
+            self.front_end = context.socket(zmq.XSUB)
+            self.front_end.bind(f"tcp://*:{self.in_bound}")
+            self.back_end = context.socket(zmq.XPUB)
+            self.back_end.setsockopt(zmq.XPUB_VERBOSE, 1)
+            self.back_end.bind(f"tcp://*:{self.out_bound}")            
+            print(
+                f"Broker {self.ip} leader: in_bound={self.in_bound}, out_bound={self.out_bound}")            
+            zmq.proxy(self.front_end, self.back_end)
+        else:
+            print("Can I run start recursively?")
+            # self.start()
             
             
 
@@ -130,6 +167,9 @@ class Publisher():
         self.zk = start_kazoo_client()
         self.ip = get_ip()
         self.socket = context.socket(zmq.PUB)
+        self.flag = False
+
+
 
     def start(self):
         print(f"Publisher: {self.ip}")
@@ -145,11 +185,16 @@ class Publisher():
             @self.zk.DataWatch(self.proxy_path)
             def proxy_watcher(data, stat):
                 print(f"Publisher: proxy watcher triggered. data:{data}")
+                # self.socket.shutdown(socket.SHUT_RDWR)
+                if self.flag is True:
+                    self.socket.close()
+                    self.socket = context.socket(zmq.PUB)
                 if data is not None:
                     intf = data.decode('utf-8')
                     conn_str = f'tcp://{intf}:{self.port}'
                     print(f"connecting: {conn_str}")
                     self.socket.connect(conn_str)
+                    self.flag = True
 
         else:  # FLOOD MODE
             conn_str = f'tcp://{self.ip}:{self.port}'
@@ -174,7 +219,7 @@ class Publisher():
             while monitor.poll():
                 evt = recv_monitor_message(monitor)
                 evt.update({'description': evt_map[evt['event']]})
-                print("Event: {}".format(evt))
+                print("Event1: {}".format(evt))
                 if evt['event'] == zmq.EVENT_MONITOR_STOPPED:
                     break
             monitor.close()
